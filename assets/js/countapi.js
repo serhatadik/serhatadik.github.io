eeed10355b0a0b33026eb9b997b5e9a6946470ec
Serhat Tadik <serhatadik@gmail.com>
2024-02-28 17:57:25 -0500

    add visitor count

--- assets/js/countapi.js
+++ assets/js/countapi.js
@@ -1,10 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var namespace = "tadikserhat.com"; // Replace "your_namespace" with your site's domain or any unique namespace you prefer.
-    var key = "visits"; // This can be any key you like. If you want to track different pages, use different keys.
-    fetch(`https://api.countapi.xyz/hit/${namespace}/${key}`)
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('visits').innerText = `Page views: ${data.value}`;
-        })
-        .catch(error => console.error('Error accessing CountAPI:', error));
-});
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.countapi.xyz/hit/tadikserhat.com/visits");
+xhr.responseType = "json";
+xhr.onload = function() {
+    document.getElementById('visits').innerText = this.response.value;
+}
+xhr.send();
