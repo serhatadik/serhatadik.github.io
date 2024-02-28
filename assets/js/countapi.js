0a5ba13ad87d7834e7ed92078ec56fd5804ae272
Serhat Tadik <serhatadik@gmail.com>
2024-02-28 17:31:37 -0500

    add visitor count

--- assets/js/countapi.js
+++ assets/js/countapi.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-  countapi.visits('tadikserhat.com', 'visits').then((result) => {
-    console.log(`Page views: ${result.value}`);
-    const visitElement = document.getElementById('visitCount');
-    if (visitElement) {
-      visitElement.textContent = result.value;
-    }
-  });
+  const namespace = 'tadikserhat.com'; // Replace with your namespace
+  const key = 'visits'; // Replace with your key
+  fetch(`https://api.countapi.xyz/hit/${namespace}/${key}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(`Page views: ${data.value}`);
+      const visitElement = document.getElementById('visitCount');
+      if (visitElement) {
+        visitElement.textContent = data.value;
+      }
+    })
+    .catch(error => console.error('Error fetching CountAPI data:', error));
 });
